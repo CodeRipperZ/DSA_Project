6c083c5353b0bf3135b0a2a7ca95eec1fec628a1
Hashan Chandika <hashanchandika.micheal@gmail.com>
2015-03-09 16:59:01 +0530

    Update ListHouse.java

--- ListHouse.java
+++ ListHouse.java
@@ -1,132 +1,108 @@
 /*
-* ListHouse Created by C.D.A Gunawardana
+ * ListHouse Created by C.D.A Gunawardana
 */
 
+//Create Generic Listable Interface
+interface Listable<E>
+{	
+	public int compareTo(E o);		
+	public void copy(E o);
+}
 
 class ListHouse implements Listable<ListHouse>
 {
 	//Declaring the variables
-	private long LotNumber =0;
-	private String FirstName="NULL";
-	private String LastName="NULL";
-	private double Price=0.0;
-	private float SquareFeet=0.0f;
-	private int NumberOfBedrooms=0;
-
-	
+	private long lotNo = 0;
+	private String fName = "NULL";
+	private String lName = "NULL";
+	private double price = 0.0;
+	private float sqrFeet = 0.0f;
+	private int noOfBeds = 0;
 	
     	//Get Method for LotNumber
-	public long getLotnumber() 
-	{
-		return LotNumber;
-	}
-
-	//Set Method for LotNumber
-	public void setLotnumber(long lotnumber) 
-	{
-		this.LotNumber = lotnumber;
-	}
-
-	//Get Method for FirstName
-	public String getFirstName()
-	{
-  		return FirstName;
+	public long getLotNumber() {
+		return this.lotNo;
 	}
 	
- 	//Set Method for FirstName	
-	public void setFirstName(String firstname)
-	{
-
-		this.FirstName=firstname;
-	}
-
-	//Get Method for LastName
-	public String getLastName()
-	{
-  		return LastName;
+    	//Get Method for FirstName
+	public String getFirstName() {
+		return this.fName;
 	}
 	
- 	//Set Method for LastName	
-	public void setLastName(String lastname)
-	{
-
-		this.LastName=lastname;
-	}	
+    	//Get Method for LastName
+	public String getLastName() {
+		return this.lName;
+	}
 	
-	//Get Method for Price
-	public double getPrice() 
-	{
-		return Price;
+    	//Get Method for Price
+	public double getPrice() {
+		return this.price;
 	}
-
-	//Set Method for Price
-	public void setPrice(double price)
-	{
-		this.Price = price;
-	}
-
+	
 	//Get Method for SquareFeet
-	public float getSquareFeet() 
-	{
-		return SquareFeet;
+	public float getSquareFeet() {
+		return this.sqrFeet;
 	}
-
-	//Set Method for SquareFeet
-	public void setSquareFeet(float squareFeet)
-	{
-		this.SquareFeet = squareFeet;
-	}
-
+	
 	//Get Method for NumberOfBedrooms
-	public int getNumberOfBedrooms() 
-	{
-		return NumberOfBedrooms;
+	public int getNoOfBedrooms() {
+		return this.noOfBeds;
 	}
-
+	
+	//Set Method for LotNumber
+	public void setLotNumber(long lotNo) {
+		this.lotNo = lotNo;
+	}
+	
+	//Set Method for FirstName
+	public void setFirstName(String fName) {
+		this.fName = fName;
+	}
+	
+	//Set Method for LastName
+	public void setLastName(String lName) {
+		this.lName = lName;
+	}
+	
+	//Set Method for Price
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	//Set Method for SquareFeet
+	public void setSquareFeet(float sqrFeet) {
+		this.sqrFeet = sqrFeet;
+	}
+	
 	//Set Method for NumberOfBedrooms
-	public void setNumberOfBedrooms(int numberofbedrooms) 
-	{
-		this.NumberOfBedrooms = numberofbedrooms;
+	public void setNoOfBedrooms(int noOfBeds) {
+		this.noOfBeds = noOfBeds;
 	}
-	
-	
 	
 	/*
-	 * compareTo Method conditions
-	 * If the Integer is equal to the argument then 0 is returned.
-	 * If the Integer is less than the argument then -1 is returned.
-	 * If the Integer is greater than the argument then 1 is returned.
+	 * compareTo Method conditions (compares only the lotNumber)
+	 * If the ListHouse object 'o' is equal to the argument then 0 is returned.
+	 * If the ListHouse object 'o' is less than the argument then -1 is returned.
+	 * If the ListHouse object 'o' is greater than the argument then 1 is returned.
 	 * 
 	 */
 	//Implements the compareTo method in Listable interface 
-	public int compareTo(ListHouse o)
-	{	
-		if(this.LotNumber < o.LotNumber)
-			return -1;
-		if(this.LotNumber > o.LotNumber)
+	public int compareTo(ListHouse o) {
+		if(o.lotNo > this.lotNo)
 			return 1;
-			
-		return 0;	
+		if(o.lotNo < this.lotNo)
+			return -1;
+		return 0;
 	}
 	
 	//Implements the copy method in Listable interface
 	//This Method will copy data from a object one to another	
-	public void copy(ListHouse o)
-	{
-		this.LotNumber = o.LotNumber;
-		this.FirstName = o.FirstName;
-		this.LastName = o.LastName;
-		this.Price = o.Price;
-		this.SquareFeet = o.SquareFeet;
-		this.NumberOfBedrooms = o.NumberOfBedrooms;
+	public void copy(ListHouse o) {
+		this.lotNo = o.lotNo;
+		this.fName = o.fName;
+		this.lName = o.lName;
+		this.price = o.price;
+		this.sqrFeet = o.sqrFeet;
+		this.noOfBeds = o.noOfBeds;
 	}
-	
 }
-
-//Create Generic Interface Listable 
-	interface Listable<E>
-	{	
-		public int compareTo(E o);		
-		public void copy(E o);
-	}
-
